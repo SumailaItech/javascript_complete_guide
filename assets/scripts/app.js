2985cb6b3927352b3d8169b753d661ee07a62e9c
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-24 05:19:58 +0200

    Return function returns the value of a function but it does not show any visual display in the browser

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,11 @@ let currentResult = defaultResult;
 
 function addNumber(num1, num2){
     const result = num1 + num2;
-    alert('the result is '+result);
+    return result;
 }
 
-addNumber(20,10);
-currentResult = (currentResult + 10) * 3 /2 -1;
+currentResult = addNumber(20,10);
+
 let calculationDescription = `( ${defaultResult} + 10) * 3 /2 -1`;
 let errorMessage = "An error has occur";
 
