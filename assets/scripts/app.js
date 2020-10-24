167fd42bcf9453c1aabfedf08bcd070ab2822c50
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-24 13:27:53 +0200

    I splitted the function into two, to prevent code repetition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
 function addNumber(){
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, ''); 
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription); 
 }
 
 addBtn.addEventListener('click', addNumber)
