df2e9af4fa77c8a696bd1146b4d981b385f73db9
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-24 13:48:21 +0200

    connected all the buttons to their various functions and has added an event listener to all the buttons in the page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,44 @@ let currentResult = defaultResult;
 function getUserNumberInput(){
     return parseInt(userInput.value);
 }
-function addNumber(){
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult + enteredNumber;
+
+function createAndWriteOutput(operator,resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription); 
 }
 
+function addNumber(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult; 
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+',initialResult,enteredNumber);
+    
+}
+
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber);
+}
+
+function multiple(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber);
+
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber);
+}
+
 addBtn.addEventListener('click', addNumber)
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiple);
+divideBtn.addEventListener('click',divide);
 
