69db0e914c854aa36d932278d06de41274c26958
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-24 05:11:23 +0200

    Learnt About how to create a function and how to call a function, a functi on can either have a parameter or not in it's creation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,14 @@
 
+
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function addNumber(num1, num2){
+    const result = num1 + num2;
+    alert('the result is '+result);
+}
+
+addNumber(20,10);
 currentResult = (currentResult + 10) * 3 /2 -1;
 let calculationDescription = `( ${defaultResult} + 10) * 3 /2 -1`;
 let errorMessage = "An error has occur";
